0004ba61cc1b2449db02f81f1ba0a476f7b2c996
ElodieCara <102527658+ElodieCara@users.noreply.github.com>
2023-10-06 10:57:29 +0200

    Maj header

--- pink-project/src/layout/Header.jsx
+++ pink-project/src/layout/Header.jsx
@@ -4,21 +4,32 @@ import Cloud from "../assets/Cloud.svg";
 
 export default function Header() {
   return (
-    <header>
+    <header className="header">
       <Navbar />
-      <div className="header__title">
-        <img src={Logo} alt="Logo Alifey et ses nuages" />
-        <h1>Alifey</h1>
-        {/* <div className="header__title__cloud">
-          <img src={Cloud} alt="" />
-        </div> */}
+      <div className="header__container">
+        <div className="header__container__title">
+          <img
+            className="header__container__title__icon--air"
+            src={Logo}
+            alt="Logo Alifey et ses nuages"
+          />
+          <h1>Alifey</h1>
+          <img
+            className="header__container__title__icon--cloud"
+            src={Cloud}
+            alt=""
+          />
+        </div>
+        <div className="header__container__quote">
+          <blockquote>
+            <p>
+              "Il faut donc être au-dessus des nuages pour <br />
+              que nos yeux s'y baignent..."
+            </p>
+            <cite>Jean Cocteau</cite>
+          </blockquote>
+        </div>
       </div>
-      <blockquote>
-        <p>
-          Il faut donc être au-dessus des nuages pour que nos yeux s'y baignent
-        </p>
-        <cite>Jean Cocteau</cite>
-      </blockquote>
     </header>
   );
 }
