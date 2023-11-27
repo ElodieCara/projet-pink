5e093408d9b4aa4ea5d6c4bd44d61e955cb4175d
ElodieCara <102527658+ElodieCara@users.noreply.github.com>
2023-11-27 15:17:31 +0100

    maj vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/Projet-Pink/",
+  base: "/Projet-Pink/",
   plugins: [react()],
   resolve: {
     alias: {
